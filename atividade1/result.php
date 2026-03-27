27749696032098612d9bdf615d879f4f91ef7abe
kauan silva <kr686729@gmail.com>
2026-03-27 20:18:39 -0300

    Update result.php

--- atividade1/result.php
+++ atividade1/result.php
@@ -1,36 +1,16 @@
 <?php
+$peso= $_POST['peso'];
+$altura= $_POST['altura'];
+$imc= $peso / ($altura * $altura);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    $peso = $_POST['peso'];
-    $altura = $_POST['altura'];
-
-    if (!empty($peso) && !empty($altura)) {
-        
-   
-        $peso = str_replace(',', '.', $peso);
-        $altura = str_replace(',', '.', $altura);
-
-  
-        $imc = $peso / ($altura * $altura);
-
-        echo "<h2>Resultado do Cálculo</h2>";
-        echo "Peso informado: " . $peso . "kg<br>";
-        echo "Altura informada: " . $altura . "m<br>";
-        echo "Seu IMC é: " . number_format($imc, 2, ',', '.') . "<br>";
-
-   
-        if ($imc < 18.5) {
-            echo "Status: Abaixo do peso";
-        } elseif ($imc >= 18.5 && $imc <= 24.9) {
-            echo "Status: Peso ideal (parabéns!)";
-        } elseif ($imc >= 25 && $imc <= 29.9) {
-            echo "Status: Levemente acima do peso";
-        } else {
-            echo "Status: Obesidade";
-        }
-    } else {
-        echo "Por favor, preencha todos os campos!";
-    }
+if ($imc >=18.5){
+    echo "voce esta magro " . $imc;
 }
+else if ($imc <= 18.6 and $imc >=24.9){
+    echo "voce esta bem";
+}
+else if ($imc <= 25){
+    echo "voce esta gordo";
+}
+
 ?>
