00aa0bfe44778548574af4bf2d1fce6ff13cbe54
kauan silva <kr686729@gmail.com>
2026-03-27 20:26:16 -0300

    Update currency converter title and input name

--- atividade3/index.php
+++ atividade3/index.php
@@ -2,13 +2,13 @@
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <title>Conversor de Moedas - Cotação 20/03</title>
+  <title>Conversor de Moedas</title>
 </head>
 <body>
 <form method="post" action="result.php">
-  <h2>Conversor de Moedas (Cotação de 20/03)</h2>
-    <label>Valor em Reais (R$):</label><br>
-    <input type="text" name="valor_reais">
+  
+    <label>Valor em Reais (R$):</label>
+    <input type="text" name="valorreais">
     
     <input type="submit" value="Converter Valores">
   </form>
