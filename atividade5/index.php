71bf30432dbdb844ae7197df8c0f3796d2f2e2cc
kauan silva <kr686729@gmail.com>
2026-03-27 20:47:11 -0300

    Update index.php

--- atividade5/index.php
+++ atividade5/index.php
@@ -2,18 +2,18 @@
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <title>Cálculo de Salário Líquido</title>
+  <title>Calculo de Salario Liquido</title>
 </head>
 <body>
     <form method="post" action="result.php">
-  <h2>Cálculo de Salário Líquido (INSS)</h2>
-    <label>Nome do Funcionário:</label><br>
-    <input type="text" name="funcionario" required><br><br>
+  <h2>Calculo de Salario Liquido (INSS)</h2>
+    <label>Nome do Funcionario:</label>
+    <input type="text" name="funcionario">
     
-    <label>Salário Bruto (R$):</label><br>
-    <input type="number" step="0.01" name="salario_bruto" required><br><br>
+    <label>Salario Bruto (R$):</label>
+    <input type="number" step="0.01" name="salario_bruto">
     
-    <input type="submit" value="Calcular Salário Líquido">
+    <input type="submit" value="Calcular Salario Liquido">
   </form>
 </body>
 </html>
