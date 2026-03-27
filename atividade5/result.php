0a8ccc9f310536f8cedc6e91b75e5210adf3fba9
kauan silva <kr686729@gmail.com>
2026-03-27 20:58:24 -0300

    Update salary calculation in result.php
    Fix syntax errors and improve salary calculation logic.

--- atividade5/result.php
+++ atividade5/result.php
@@ -1,4 +1,15 @@
 <?php
-$
+$salariobruto= $_POST['Salario_Bruto'];
+$salariominimo= 3242;
+
+$abaixo2minimo= $salariobruto - ( $salariobruto * 8 / 100;
+$acima2minimo= $salariobruto - ( $salariobruto * 10 / 100;
+
+if ($salariobruto >= $salariominimo){
+  echo "salario: " . $$abaixo2minimo;
+}
+else if ($salariobruto < $salariominimo){
+  echo "salario: " . $acima2minimo;
+}
 
 ?>
