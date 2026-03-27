677d36afe841d2eaac611dcad89f9696fa004bdb
kauan silva <kr686729@gmail.com>
2026-03-27 20:39:53 -0300

    Update result.php

--- atividade3/result.php
+++ atividade3/result.php
@@ -1,29 +1,12 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$reais= $_POST['valorreais'];
+$euros= 5.41;
+$dolares= 4.97;
+$libras= 6.33;
 
-    $reais = str_replace(',', '.', $_POST['valor_reais']);
+$euro= $reais * $euros;
+$dolar= $reais * $dolares;
+$libra= $reais * $libras;
 
-    if (is_numeric($reais) && $reais > 0) {
-       
-        $cotacao_dolar = 4.97;
-        $cotacao_euro = 5.41;
-        $cotacao_libra = 6.33;
-
-       
-        $dolar = $reais / $cotacao_dolar;
-        $euro = $reais / $cotacao_euro;
-        $libra = $reais / $cotacao_libra;
-
-        echo "<h2>Resultado da Conversão</h2>";
-        echo "Valor original: <strong>R$ " . number_format($reais, 2, ',', '.') . "</strong><br><hr>";
-        
-        echo "Dólar (USD): $ " . number_format($dolar, 2, '.', ',') . "<br>";
-        echo "Euro (EUR): € " . number_format($euro, 2, ',', '.') . "<br>";
-        echo "Libra (GBP): £ " . number_format($libra, 2, ',', '.') . "<br>";
-        
-        echo "<br><small>*Cotações baseadas no fechamento de 20/03.</small>";
-    } else {
-        echo "Por favor, coloque um valor numérico válido.";
-    }
-}
+echo "euro " . $euro . "dolar " . $dolar . "libra " . $libra;
 ?>
